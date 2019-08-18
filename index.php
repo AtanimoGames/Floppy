bfad0a0cfda7a572034626351bd8d47d0fd0ef07
AtanimoGames <46455759+AtanimoGames@users.noreply.github.com>
2019-08-18 17:45:23 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+﻿<?php
+require_once 'config/config.php';
+getHeader();
+?>
+      <center>
+      <object width="100%" height="100%">
+         <embed src="homepage.swf" width="1200" height="700" style="margin-top: 30px !important;border: 3px #3da5da solid;box-shadow: 0 0 3px #000000c2;">
+      </object>
+</center>
+</body></html>
